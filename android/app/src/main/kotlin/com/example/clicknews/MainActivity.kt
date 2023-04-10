5183dc241511fb9641b2d53c7ef873ac9e634477
unknown <akhilkholia9@gmail.com>
2023-04-10 16:32:34 +0530

    news

---
+++ android/app/src/main/kotlin/com/example/clicknews/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.clicknews
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/constants.dart
@@ -0,0 +1,1 @@
+const String apiKey = '85940a4d7b23488ba7ecd9e9e7c6533e';
